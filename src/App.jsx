a59519532305437c97162e33e2ca6aef1588d1ef
Muhammad Mursleen Mamoon <mursleenmamoon@gmail.com>
2026-07-02 01:40:31 +0500

    Fix import path for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Programs from './components/programs/Programs'
 import Title from './components/title/Title'
